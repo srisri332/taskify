c9f39fc2c23e66fcd8ecdf42117da7bb60d53790
sri sri <srisrireddy.k@gmail.com>
2020-10-22 22:16:43 +0530

    push commit

---
+++ taskify/build/precache-manifest.47b09750abc82c055276a7acd9d39a01.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e798a91bc82a560291c0bfb4a493e30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75aad5bf3d754dba2fa5",
+    "url": "/static/css/main.538f8457.chunk.css"
+  },
+  {
+    "revision": "62a432e8b2d9bf2e04ad",
+    "url": "/static/js/2.630390ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.630390ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75aad5bf3d754dba2fa5",
+    "url": "/static/js/main.ecee2d5e.chunk.js"
+  },
+  {
+    "revision": "a61f90c652c2d9640aca",
+    "url": "/static/js/runtime-main.3f40ae63.js"
+  },
+  {
+    "revision": "fa5a16607b99df8ce65a22f6b3f6e346",
+    "url": "/static/media/close.fa5a1660.png"
+  },
+  {
+    "revision": "80552311adbc1f7e496d72c128c352a4",
+    "url": "/static/media/find.80552311.png"
+  },
+  {
+    "revision": "6329161075decaa826e0f1447b6617cd",
+    "url": "/static/media/settings (1).63291610.png"
+  },
+  {
+    "revision": "805ac484620330d3659dc46ee2dc5373",
+    "url": "/static/media/user.805ac484.png"
+  }
+]);
